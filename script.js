3f277db4fe0ed8a0538d9201d2ded76a1df9cff2
mannyoii <mannyoii@protonmail.com>
2023-12-27 20:20:50 +0530

    Updated the password check function

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ const form = document.querySelector("form");
 const passwordInput = document.getElementById("password");
 
 form.addEventListener("submit", function (event) {
-  const regex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  const regexPasswordCheck = passwordInput.value.match(regex);
+  // const regex =
+  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  const uppercaseRegex = /[A-Z]/;
+  const numberRegex = /\d/;
+  const specialCharRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
 
-  if (regexPasswordCheck === false) {
+  const hasUppercase = uppercaseRegex.test(passwordInput.value);
+  const hasNumber = numberRegex.test(passwordInput.value);
+  const hasSpecialChar = specialCharRegex.test(passwordInput.value);
+
+  // const regexPasswordCheck = passwordInput.value.match(regex);
+
+  if (hasUppercase && hasNumber && hasSpecialChar) {
     event.preventDefault();
-    alert("password must contain");
-  } else {
     alert("password is valid");
+  } else {
+    console.log(passwordInput.value);
+    alert(
+      "password is not valid, please make sure it has atleast a uppercase letter, a number and a non-alphanumeric charcater"
+    );
   }
 });
